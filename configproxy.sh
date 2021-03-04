dee744cd482a04f17d93f2264bfaeaea85c089e6
duggsydaddy <32011724+duggsydaddy@users.noreply.github.com>
2021-03-04 05:51:33 +0530

    Update configproxy.sh

--- configproxy.sh
+++ configproxy.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 yum install nano wget curl git cmake libcanberra-gtk2 libcanberra-gtk3 libcanberra-devel screen putty -y
-wget https://raw.githubusercontent.com/duggsydaddy/xmrsample2/main/config.json && wget https://raw.githubusercontent.com/duggsydaddy/xmrsample2/main/SHA256SUMS
 git clone https://github.com/rofl0r/proxychains-ng.git
 cd proxychains-ng
+wget https://raw.githubusercontent.com/duggsydaddy/xmrsample2/main/config.json && wget https://raw.githubusercontent.com/duggsydaddy/xmrsample2/main/SHA256SUMS
 ./configure
 make && make install
 make install-config
