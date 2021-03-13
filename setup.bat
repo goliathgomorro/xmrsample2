91626268ce6084426b299d60737477df44c3311e
duggsydaddy <32011724+duggsydaddy@users.noreply.github.com>
2021-03-14 01:27:18 +0530

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -5,6 +5,7 @@ echo Copy NGROK to System32...
 copy ngrok.exe C:\Windows\System32
 echo CONNECT NGROK AUTH TOKEN...
 ngrok authtoken "1piLsET12uJXZwVah0gsqoO8ghb_3N9tu9LtjRwrUtmXgJh9F"
-ngrok tcp 3389
+start "" ngrok tcp 3389
 echo "done"
+tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Caaaa"
 NGROK-CHECK.bat
