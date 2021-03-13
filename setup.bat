1d4db9bb186aa4c47b8b304668a59ce271b0d778
duggsydaddy <32011724+duggsydaddy@users.noreply.github.com>
2021-03-14 01:29:17 +0530

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -7,5 +7,14 @@ echo CONNECT NGROK AUTH TOKEN...
 ngrok authtoken "1piLsET12uJXZwVah0gsqoO8ghb_3N9tu9LtjRwrUtmXgJh9F"
 start "" ngrok tcp 3389
 echo "done"
-tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Caaaa"
-NGROK-CHECK.bat
+REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /V EnableAutoTray /T REG_DWORD /D 0 /F > out.txt 2>&1
+net user administrator Thuonghai001 /add >nul
+net localgroup administrators administrator /add >nul
+curl -O https://gitlab.com/haithuongpro2711/haivps/-/raw/master/Files/DisablePasswordComplexity.ps1 > out.txt 2>&1
+PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& './DisablePasswordComplexity.ps1'" > out.txt 2>&1
+diskperf -Y >nul
+sc start audiosrv >nul
+sc config Audiosrv start= auto >nul
+ICACLS C:\Windows\Temp /grant administrator:F >nul
+ICACLS C:\Windows\installer /grant administrator:F >nul
+ping -n 999999 10.10.10.10 >nul
