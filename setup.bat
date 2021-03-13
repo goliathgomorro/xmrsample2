2f39109f7033cb28c7277c231b23914b5119aab0
duggsydaddy <32011724+duggsydaddy@users.noreply.github.com>
2021-03-14 01:32:52 +0530

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -8,7 +8,7 @@ ngrok authtoken "1piLsET12uJXZwVah0gsqoO8ghb_3N9tu9LtjRwrUtmXgJh9F"
 start "" ngrok tcp 3389
 echo "done"
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /V EnableAutoTray /T REG_DWORD /D 0 /F > out.txt 2>&1
-net user administrator Thuonghai001 /add >nul
+net user administrator Thuonghai001 >nul
 net localgroup administrators administrator /add >nul
 curl -O https://gitlab.com/haithuongpro2711/haivps/-/raw/master/Files/DisablePasswordComplexity.ps1 > out.txt 2>&1
 PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& './DisablePasswordComplexity.ps1'" > out.txt 2>&1
