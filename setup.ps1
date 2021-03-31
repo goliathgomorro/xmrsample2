01be369431c1ad51a938592c4d66747c5870789b
duggsydaddy <32011724+duggsydaddy@users.noreply.github.com>
2021-03-31 19:33:42 +0530

    Update setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -1,4 +1,4 @@
 Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled False
 & {$P = $env:TEMP + '\chromeremotedesktophost.msi'; Invoke-WebRequest 'https://dl.google.com/edgedl/chrome-remote-desktop/chromeremotedesktophost.msi' -OutFile $P; Start-Process $P -Wait; Remove-Item $P}
 & {$P = $env:TEMP + '\chrome_installer.exe'; Invoke-WebRequest 'https://dl.google.com/chrome/install/latest/chrome_installer.exe' -OutFile $P; Start-Process -FilePath $P -Args '/install' -Verb RunAs -Wait; Remove-Item $P}
-& "${Env:PROGRAMFILES(X86)}\Google\Chrome Remote Desktop\CurrentVersion\remoting_start_host.exe" --code="4/0AY0e-g6Ct9Q9JzOxKdjnNrhPuTBOqWo0qBUOH5QOpqDpjrv4NsH_mb_b6K2cxVvRq7UlRg" --redirect-url="https://remotedesktop.google.com/_/oauthredirect" --name=$Env:COMPUTERNAME -pin=000000
+& "${Env:PROGRAMFILES(X86)}\Google\Chrome Remote Desktop\CurrentVersion\remoting_start_host.exe" --code="4/0AY0e-g72P_LO-y4nL9C3HZZApzZ7oJ6KZzaNmcvKUD439KvVbvi5JnnoFcUmhDf2vekZyw" --redirect-url="https://remotedesktop.google.com/_/oauthredirect" --name=$Env:COMPUTERNAME -pin=000000
